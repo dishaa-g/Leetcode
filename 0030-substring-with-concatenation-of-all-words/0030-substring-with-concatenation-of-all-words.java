7a931541c8ec7b4ecfdbf0e75e6097fd2fe8456b
Disha Gupta <132289717+dishaa-g@users.noreply.github.com>
2024-11-11 13:03:58 +0530

    Time: 11 ms (97.24%), Space: 45.4 MB (70.37%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) {
+            return result;
+        }
+
+        int wordLength = words[0].length();
+        int numWords = words.length;
+        int totalLength = wordLength * numWords;
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLength; i++) {
+            Map<String, Integer> seenWords = new HashMap<>();
+            int left = i, right = i, count = 0;
+            
+            while (right + wordLength <= s.length()) {
+                String word = s.substring(right, right + wordLength);
+                right += wordLength;
+
+                if (wordCount.containsKey(word)) {
+                    seenWords.put(word, seenWords.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (seenWords.get(word) > wordCount.get(word)) {
+                        String leftWord = s.substring(left, left + wordLength);
+                        seenWords.put(leftWord, seenWords.get(leftWord) - 1);
+                        left += wordLength;
+                        count--;
+                    }
+
+                    if (count == numWords) {
+                        result.add(left);
+                    }
+                } else {
+                    seenWords.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
