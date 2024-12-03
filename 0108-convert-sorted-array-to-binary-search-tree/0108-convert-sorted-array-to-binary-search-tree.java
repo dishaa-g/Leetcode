693e37c83e3dcb7194decab236528a41b78e3a7d
Disha Gupta <132289717+dishaa-g@users.noreply.github.com>
2024-12-03 21:23:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (38.45%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,45 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildTree(nums, left, mid - 1);
+        root.right = buildTree(nums, mid + 1, right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root1 = solution.sortedArrayToBST(new int[] {-10, -3, 0, 5, 9});
+        TreeNode root2 = solution.sortedArrayToBST(new int[] {1, 3});
+
+        printTree(root1);
+        System.out.println();
+        printTree(root2);
+    }
+
+    private static void printTree(TreeNode root) {
+        if (root == null) return;
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+}
