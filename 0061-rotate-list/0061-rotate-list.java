1f4829476962ee4ec4acadddb97282d8c10f9f45
Disha Gupta <132289717+dishaa-g@users.noreply.github.com>
2025-04-01 15:48:24 +0530

    Time: 0 ms (100%), Space: 42.8 MB (36.06%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -15,15 +15,19 @@ class Solution {
         }
         ListNode curr= head;
         int length=1;
+        // list ka length nikalne
         while(curr.next!=null){
             curr=curr.next;
             length++;
         }
+        //idhr last node ko first node se connect krdiya 
         curr.next=head;
+        // number of skips calculate krne
         int n= length- (k%length);
         for(int i=0;i<n;i++){
             curr=curr.next;
         }
+        //loop me jaha tk skip krna tha vha tk krke next node ko head banadiya by putting null in the last node's address
         head=curr.next;
         curr.next=null;
         return head;
