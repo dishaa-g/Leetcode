5eec42c32e05ce0ce2b1781ef51536f893ba80e2
Disha Gupta <132289717+dishaa-g@users.noreply.github.com>
2025-06-25 00:11:50 +0530

    Time: 244 ms (6.94%), Space: 46.4 MB (35.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n= nums.length;
+        int count=0;
+        int candidate=0;
+        int result=0;
+        for(int i=0;i<n;i++){
+            candidate=nums[i];
+            for(int j=0;j<n;j++){
+                if(nums[j]==candidate){
+                    count++;
+                }
+                if(count==2){
+                    count=0;
+                    break;
+                }
+            }
+            if(count==1){
+                result=candidate;
+                break;
+            }
+        }
+        return result;
+    }
+}
